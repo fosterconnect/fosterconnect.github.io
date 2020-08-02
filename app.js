236dc6d244d59c3ba7b80d811812353da31e6106
Snipta Mallick <snipta_mallick@affirmednetworks.com>
2020-08-01 20:30:51 -0500

    npm install and config

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+app.use(express.static('public'));
+
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/events",function(req,res){
+    res.sendFile(path + "events.html");
+});
+
+router.get("/programs",function(req,res){
+    res.sendFile(path + "programs.html");
+});
+
+router.get("/getinvolved",function(req,res){
+    res.sendFile(path + "getinvolved.html");
+})
+
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
